16e3f9ceb7445000a6dd137c1e51cdf8c17dc191
Alexander Cardozo Alaca <150977303+cardozoaa@users.noreply.github.com>
2024-09-29 12:23:02 +0000

    segundo commit

---
+++ home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+#agregar configuracion de la pagina
+st.set_page_config(page_title='TP - Maestria en Ciencia de Datos', layout='wide')
+
+st.title('Trabajo Practico - Maestria en Ciencia de Datos')
+
