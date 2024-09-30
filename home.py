fb361333c66fb1cddfb7a5cfe4925225b5bea135
Alexander Cardozo Alaca <150977303+cardozoaa@users.noreply.github.com>
2024-09-30 22:45:15 +0000

    sexto commit

--- home.py
+++ home.py
@@ -5,3 +5,7 @@ import streamlit as st
 st.set_page_config(page_title='TP - Maestria en Ciencia de Datos', layout='wide')
 
 st.title('Trabajo Practico - Maestria en Ciencia de Datos')
+st.subheader('Integrantes:')
+st.write('Alexander Cardozo')
+st.write('Favio Lopez')
+st.write('Fredy Barrios')
