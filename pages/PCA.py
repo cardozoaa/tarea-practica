7f7967b4f915028c5dceee9854ce64930421db74
Alexander Cardozo Alaca <150977303+cardozoaa@users.noreply.github.com>
2024-10-02 14:23:48 +0000

    PCA-corregido

--- pages/PCA.py
+++ pages/PCA.py
@@ -44,7 +44,8 @@ else:
     st.warning('No se ha cargado ningun archivo')
 
 
-opciones = ['Analisis Exploratorio']
+opciones = ['Analisis Exploratorio',
+            'PCA']
 
 # Seleccionar opcion
 opcion = st.sidebar.radio('Selecciona una Opcion',opciones)
@@ -90,7 +91,7 @@ if opcion == 'Analisis Exploratorio':
 
 
         # Eliminar columnas categoricas
-        st.subheader('Eliminar Columnas')
+        st.subheader('Eliminar Columnas Categóricas')
         lista_columnas = df.columns
         columnas = st.multiselect('Seleccione las columnas a eliminar', lista_columnas)
 
@@ -129,15 +130,37 @@ if opcion == 'Analisis Exploratorio':
                     st.write(df_normalizado.head())
         else:
             st.info('No se han normalizado los datos')
-            st.write(st.session_state.df_normalizado.head())
+            st.session_state.df = df
+
+elif opcion == 'PCA':
+    # PCA
+    st.subheader('PCA')
+    if 'df_normalizado' in st.session_state:
+        df_normalizado = st.session_state.df_normalizado
+
+        pca = PCA()
+        componentes = pca.fit_transform(df_normalizado)
+
+        st.write('Varianza explicada', pca.explained_variance_)
+        st.write('Varianza explicada por cada componente:', pca.explained_variance_ratio_)
+
+        varianza_acumulada = np.cumsum(pca.explained_variance_ratio_)
+        st.write('Varianza explicada acumulada:', varianza_acumulada)
+
+        fig=plt.figure(figsize=(7,7))
+        plt.plot(np.arange(1, len(varianza_acumulada) + 1), varianza_acumulada)# Cambiar la numeración del eje X
+        plt.hlines(y=0.9,xmin=1,xmax=len(varianza_acumulada) + 1,colors='r',linestyles='--')
+        plt.xlabel('Numero de Componentes Principales')
+        plt.ylabel('Varianza Acumulada')
+        st.pyplot(fig)
+    else:
         
-        # PCA
-        st.subheader('PCA')
-        if 'df_normalizado' in st.session_state:
-            df_normalizado = st.session_state.df_normalizado
+        if 'df' in st.session_state:
+            st.info('Datos sin normalizar')
+            df = st.session_state.df
 
             pca = PCA()
-            componentes = pca.fit_transform(df_normalizado)
+            componentes = pca.fit_transform(df)
 
             st.write('Varianza explicada', pca.explained_variance_)
             st.write('Varianza explicada por cada componente:', pca.explained_variance_ratio_)
@@ -151,7 +174,4 @@ if opcion == 'Analisis Exploratorio':
             plt.xlabel('Numero de Componentes Principales')
             plt.ylabel('Varianza Acumulada')
             st.pyplot(fig)
-        else:
-            st.warning('Primero normalice los datos')
-    else:
-        st.warning('No se ha cargado ningun archivo')
+        
